c4ba715a2d56961c825413e99abef17c7068fcb3
Chris Greene <chris.greene@justin.tv>
2012-03-16 15:29:42 -0700

    Finished separating low pass from high pass

--- filters.cpp
+++ filters.cpp
@@ -12,14 +12,8 @@ void highPassTimeStep(Circuit &c, int reduction){
             for(int index = 0; index < 3;index++){
                 double _Vout = c.Vout(i,j)[index];
                 double _dVin = c.Vin(ip,jp)[index]-c.Vin_prev(ip,jp)[index];
-                /* Low Pass */
-                 
-                double _dVout = c.dVout(i,j)[index];
-                double _ddVout = ((c.Vin(ip,jp)[index]
-                                    -c.Vin_prev(ip,jp)[index])
-                                    -_dVout)*iR*iC/(dt*dt);
                 /* High Pass */
-//                double _dVout = _dVin-(c.iR*c.iC)*_dVout;
+                double _dVout = _dVin-(c.iR*c.iC)*_dVout;
     
                 c.Vout(i,j)[index] = _Vout+_dVout*dt;
                 c.dVout(i,j)[index] = _dVout + _ddVout*dt;
@@ -61,7 +55,6 @@ void lowPassTimeStep(Circuit &c, int reduction){
     
                 c.Vout(i,j)[index] = _Vout+_dVout*dt;
                 c.dVout(i,j)[index] = _dVout + _ddVout*dt;
-                _Vout = c.Vin(ip,jp)[index]-c.qVout(i,j)[index];
                 if(_Vout > 255)
                 {
                     c.qVout(i,j)[index] = 255;
