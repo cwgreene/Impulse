644b4f868662d9b50912ce7757e560c2d2e800ff
Chris Greene <archgoon@gmail.com>
2014-02-15 17:17:11 -0800

    IMPULSE-10 Use boost library for parsing command line arguments
    Fixes https://github.com/cwgreene/Impulse/issues/10

--- Makefile
+++ Makefile
@@ -1,9 +1,9 @@
 all: filters.o band_pass.o
-	g++ -std=c++11 `pkg-config --cflags opencv` band_pass.o filters.o -O2 -o band_pass `pkg-config --libs opencv` -lboost_system -lboost_filesystem
+	g++ -std=c++11 `pkg-config --cflags opencv` band_pass.o filters.o -O2 -o band_pass `pkg-config --libs opencv` -lboost_system -lboost_filesystem -lboost_program_options
 debug: filters.o band_pass.o
 	g++ `pkg-config --cflags opencv`  band_pass.o filters.o -O2 -o band_pass_debug `pkg-config --libs opencv`
 band_pass.o: band_pass.cpp
-	g++ -c `pkg-config --cflags opencv` band_pass.cpp -O2 
+	g++ -c -std=c++11 `pkg-config --cflags opencv` band_pass.cpp -O2 
 filters.o: filters.cpp filters.hpp
 	g++ -c `pkg-config --cflags opencv` filters.cpp -O2 
 frame_colors:	
--- band_pass.cpp
+++ band_pass.cpp
@@ -4,12 +4,15 @@
 #include <iostream>
 
 #include <boost/filesystem.hpp>
+#include <boost/program_options.hpp>
 
 #include "filters.hpp"
 
 using namespace cv;
 
-int print_vec(Vec3b v){
+namespace po = boost::program_options;
+
+void print_vec(Vec3b v){
     unsigned int a=v[0],b=v[0],c=v[0];
     std::cout << a << " " << b << " " << c << std::endl;
 }
@@ -36,22 +39,52 @@ void skipAhead(int n, VideoCapture &cap){
     }
 }
 
-int main(int argc, char **argv){
-    int reduce = 1;
-    int skip = 0;
-    if(argc < 2){
-        printf("OpenCV version %s\n", CV_VERSION);
-        printf("usage: machinelearning MOVIE_FILE\n");
-        return -1;
+void print_help(po::options_description &description,
+                po::positional_options_description &pd) {
+    std::string usage;
+    for(unsigned int i = 0; i < pd.max_total_count(); i++) {
+        usage += " ";
+        usage += pd.name_for_position(i);
     }
-    if(argc >= 3)
-        reduce = atoi(argv[2]);
-    if(argc >= 4)
-        skip = atoi(argv[3]);
-    VideoCapture cap(argv[1]);
+    std::cout << "Usage: band_pass" + usage << std::endl;
+    std::cout << description;
+    exit(-1);
+}
+
+int main(int argc, char **argv){
+    int skip;
+    int reduce;
+    po::options_description description;
+    description.add_options()
+                            ("help", "Display this help message")
+                            ("reduce", po::value<int>(&reduce)->default_value(1), "Reduction factor")
+                            ("skip", po::value<int>(&skip)->default_value(0), "Frame skip")
+                            ("input-file", "Location of movie");
+
+    po::positional_options_description pd;
+    pd.add("input-file", 1);
+
+    // Parse
+    po::variables_map vm;
+    po::store(po::command_line_parser(argc, argv).
+                options(description).positional(pd).run(), vm);
+    po::notify(vm);
+
+
+    if(vm.count("help")) {
+        print_help(description, pd);
+    }
+
+    if(!vm.count("input-file")) {
+        print_help(description, pd);
+    }
+
+    std::string input_file = vm["input-file"].as<std::string>();
+
+    VideoCapture cap(input_file);
     if (!cap.isOpened()) {
-        std::cout << "Failed to open " << argv[1] << std::endl;
-        if (!boost::filesystem::exists(argv[1])) {
+        std::cout << "Failed to open " << input_file << std::endl;
+        if (!boost::filesystem::exists(input_file)) {
             std::cout << "File does not exist." << std::endl;;
         } else {
             std::cout << "Filetype not supported, make sure that OpenCV has"
@@ -74,13 +107,13 @@ int main(int argc, char **argv){
 
     skipAhead(skip, cap);
 
-    copyEach(frame,circuit2.qVout,reduce);
+    copyEach(frame,circuit2.qVout, reduce);
 
     //frame.copyTo(circuit.qVout);
     copyEach(frame,circuit2.Vin, 1);
     highPassTimeStep(circuit2, reduce);
-    copyEach(circuit2.Vout,circuit1.Vin,reduce);
-    copyEach(circuit1.Vin,circuit1.Vin_prev,reduce);
+    copyEach(circuit2.Vout,circuit1.Vin, reduce);
+    copyEach(circuit1.Vin,circuit1.Vin_prev, reduce);
 
     int peekx= 100;
     int peeky= 100;
@@ -96,7 +129,7 @@ int main(int argc, char **argv){
 
         std::cout << (int)frame(peekx,peeky)[1] << " ";
         copyEach(frame,circuit2.Vin, 1);
-        highPassTimeStep(circuit2,reduce);
+        highPassTimeStep(circuit2, reduce);
         copyEach(circuit1.Vin,circuit1.Vin_prev, reduce);
         copyEach(circuit2.Vout,circuit1.Vin, reduce);
         lowPassTimeStep(circuit2,reduce);
